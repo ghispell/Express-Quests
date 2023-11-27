c9b8ec901ebdde941330405c26972058aa705e7b
ghispell <ghis.pell@hotmail.fr>
2023-11-27 16:22:47 +0100

    Api user update2

--- src/app.js
+++ src/app.js
@@ -3,9 +3,9 @@ const express = require("express");
 const app = express();
 
 const movieControllers = require("./controllers/movieControllers");
-
+const userControllers = require("./controllers/userControllers");
 app.get("/api/movies", movieControllers.getMovies);
 app.get("/api/movies/:id", movieControllers.getMovieById);
-app.get("/api/users", movieControllers.getUsers);
-app.get("/api/users/:id", movieControllers.getUsersById);
+app.get("/api/users", userControllers.getUsers);
+app.get("/api/users/:id", userControllers.getUsersById);
 module.exports = app;
--- src/controllers/movieControllers.js
+++ src/controllers/movieControllers.js
@@ -57,38 +57,8 @@ const getMovieById = (req, res) => {
     });
 };
 
-const getUsers = (req, res) => {
-  database
-    .query("select * from users")
-    .then(([users]) => {
-      res.json(users);
-    })
-    .catch((err) => {
-      console.error(err);
-      res.sendStatus(500);
-    });
-};
-
-const getUsersById = (req, res) => {
-  const id = parseInt(req.params.id);
-  database
-    .query("select * from users where id=?", [id])
-    .then(([users]) => {
-      if (users[0] != null) {
-        res.json(users[0]);
-      } else {
-        res.sendStatus(404);
-      }
-    })
-    .catch((err) => {
-      console.error(err);
-      res.sendStatus(500);
-    });
-};
 
 module.exports = {
   getMovies,
   getMovieById,
-  getUsers,
-  getUsersById,
 };
