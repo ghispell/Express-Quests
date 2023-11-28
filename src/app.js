64d48e7ed60489d6849898b7578c332c3dec1393
ghispell <ghis.pell@hotmail.fr>
2023-11-28 08:41:29 +0100

    Added POST route for users

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 
 const app = express();
+app.use(express.json());
 
 const movieControllers = require("./controllers/movieControllers");
 const userControllers = require("./controllers/userControllers");
@@ -8,4 +9,6 @@ app.get("/api/movies", movieControllers.getMovies);
 app.get("/api/movies/:id", movieControllers.getMovieById);
 app.get("/api/users", userControllers.getUsers);
 app.get("/api/users/:id", userControllers.getUsersById);
+app.post("/api/movies", movieControllers.postMovie);
+app.post("/api/users", userControllers.postUser);
 module.exports = app;
--- src/controllers/movieControllers.js
+++ src/controllers/movieControllers.js
@@ -1,31 +1,21 @@
-const movies = [
-  {
-    id: 1,
-    title: "Citizen Kane",
-    director: "Orson Wells",
-    year: "1941",
-    color: false,
-    duration: 120,
-  },
-  {
-    id: 2,
-    title: "The Godfather",
-    director: "Francis Ford Coppola",
-    year: "1972",
-    color: true,
-    duration: 180,
-  },
-  {
-    id: 3,
-    title: "Pulp Fiction",
-    director: "Quentin Tarantino",
-    year: "1994",
-    color: true,
-    duration: 180,
-  },
-];
-
 const database = require("../../database");
+
+const postMovie = (req, res) => {
+  const { title, director, year, color, duration } = req.body;
+
+  database
+    .query(
+      "INSERT INTO movies(title, director, year, color, duration) VALUES (?, ?, ?, ?, ?)",
+      [title, director, year, color, duration]
+    )
+    .then(([result]) => {
+      res.status(201).send({ id: result.insertId });
+    })
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(500);
+    });
+};
 
 const getMovies = (req, res) => {
   database
@@ -57,8 +47,8 @@ const getMovieById = (req, res) => {
     });
 };
 
-
 module.exports = {
   getMovies,
   getMovieById,
+  postMovie,
 };
