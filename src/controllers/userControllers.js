e99e46942b55758d9237b47d73a86b13e4f78a93
ghispell <ghis.pell@hotmail.fr>
2023-11-29 10:41:13 +0100

    Quete 3

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -1,6 +1,6 @@
 const database = require("../../database");
 
-const postUser = (req, res) => {
+const postUsers = (req, res) => {
   const { firstname, lastname, email, city, language } = req.body;
 
   database
@@ -46,8 +46,30 @@ const getUsersById = (req, res) => {
     });
 };
 
+const updateUsers = (req, res) => {
+  const id = parseInt(req.params.id);
+  const { firstname, lastname, email, city, language } = req.body;
+
+  database
+    .query(
+      "update users set firstname =?, lastname = ?, email = ?, city = ?, language = ? where id = ?",
+      [firstname, lastname, email, city, language, id]
+    )
+    .then(([result]) => {
+      if (result.affectedRows === 0) {
+        res.sendStatus(404);
+      } else {
+        res.sendStatus(204);
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(500);
+    });
+};
 module.exports = {
   getUsers,
   getUsersById,
-  postUser,
+  postUsers,
+  updateUsers,
 };
