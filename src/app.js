cac8bb1064f8549b0f335130c03abc5df373f093
ghispell <ghis.pell@hotmail.fr>
2023-11-30 10:14:38 +0100

    Express 3.2

--- src/app.js
+++ src/app.js
@@ -1,18 +1,20 @@
 const express = require("express");
-
+const movieControllers = require("./controllers/movieControllers");
+const userControllers = require("./controllers/userControllers");
 const app = express();
 app.use(express.json());
 
-const movieControllers = require("./controllers/movieControllers");
-const userControllers = require("./controllers/userControllers");
+const validateMovie = require("./middlewares/validateMovie");
+const validateUser = require("./middlewares/validateUser");
+
 app.get("/api/movies", movieControllers.getMovies);
 app.get("/api/movies/:id", movieControllers.getMovieById);
 app.get("/api/users", userControllers.getUsers);
 app.get("/api/users/:id", userControllers.getUsersById);
-app.post("/api/movies", movieControllers.postMovie);
-app.post("/api/users", userControllers.postUsers);
+app.post("/api/movies", validateMovie, movieControllers.postMovie);
+app.post("/api/users", validateUser, userControllers.postUsers);
 
-app.put("/api/movies/:id", movieControllers.updateMovie);
-app.put("/api/users/:id", userControllers.updateUsers);
+app.put("/api/movies/:id", validateMovie, movieControllers.updateMovie);
+app.put("/api/users/:id", validateUser, userControllers.updateUsers);
 
 module.exports = app;
---
+++ src/middlewares/validateMovie.js
@@ -0,0 +1,26 @@
+const Joi = require("joi");
+
+const movieSchema = Joi.object({
+  title: Joi.string().max(255).required(),
+  director: Joi.string().max(255).required(),
+  year: Joi.number().integer().min(1896).max(2030).required(),
+  color: Joi.number().integer().min(0).max(1).required(),
+  duration: Joi.number().max(700).required(),
+});
+
+const validateMovie = (req, res, next) => {
+  const { title, director, year, color, duration } = req.body;
+
+  const { error } = movieSchema.validate(
+    { title, director, year, color, duration },
+    { abortEarly: false }
+  );
+
+  if (error) {
+    res.status(422).json({ validationErrors: error.details });
+  } else {
+    next();
+  }
+};
+
+module.exports = validateMovie;
