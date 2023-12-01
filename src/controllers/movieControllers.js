1eea22ecec99ae34ab4bd8dedaf90a6774bb461f
ghispell <ghis.pell@hotmail.fr>
2023-12-01 10:16:10 +0100

    express 1.2 bis

--- src/controllers/movieControllers.js
+++ src/controllers/movieControllers.js
@@ -18,17 +18,32 @@ const postMovie = (req, res) => {
 };
 
 const getMovies = (req, res) => {
+  let sql = "select * from movies";
+  const sqlValues = [];
+
+  if (req.query.color != null) {
+    sql += " where color = ?";
+    sqlValues.push(req.query.color);
+
+    if (req.query.max_duration != null) {
+      sql += " and duration <= ?";
+      sqlValues.push(req.query.max_duration);
+    }
+  } else if (req.query.max_duration != null) {
+    sql += " where duration <= ?";
+    sqlValues.push(req.query.max_duration);
+  }
+
   database
-    .query("select * from movies")
+    .query(sql, sqlValues)
     .then(([movies]) => {
       res.json(movies);
     })
     .catch((err) => {
       console.error(err);
-      res.sendStatus(500);
+      res.status(500).send("Error retrieving data from database");
     });
 };
-
 const getMovieById = (req, res) => {
   const id = parseInt(req.params.id);
 
